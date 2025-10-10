1a9cc74c2efd8a594d4415bf11d9fdc9794c889a
Gabriel Mendes <gabriel.mendes.t0129859@edu.atec.pt>
2025-10-10 08:01:50 +0100

    Revisão do exercício

--- Exercícios/exer1.py
+++ Exercícios/exer1.py
@@ -1,9 +1,7 @@
 segs = int(input ("Insira os segundos desejados: "))
 
 horas = segs // 3600
-
 minutos = horas % 3600
-
 total_seg = segs % 60
 
-print(horas, "hora(s),", minutos, "minuto(s) e", total_seg, "segundo(s).")
+print(horas, "hora(s),", minutos, "minuto(s) e", total_seg, "segundo(s)")
