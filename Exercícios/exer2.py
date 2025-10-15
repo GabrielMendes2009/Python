1a70a11419a8c7c54c3020df584603ee71ea5584
Gabriel Mendes <gabriel.mendes.t0129859@edu.atec.pt>
2025-10-15 14:39:16 +0100

    ex2 - continuação

--- Exercícios/exer2.py
+++ Exercícios/exer2.py
@@ -3,23 +3,17 @@ num2 = int(input("Insira um número: "))
 num3 = int(input("Insira um número: "))
 
 
-if num1 >= num2 | num1 >= num3:    
-    print(f"{num1}, é maior")
-    
-elif num2 >= num1 | num2 >= num3:
-    print(f"{num2}, é maior")
-
+if num1 >= num2 & num1 >= num3:    
+    print(f"{num1} é maior")
+elif num2 >= num1 & num2 >= num3:
+    print(f"{num2} é maior")
 else:
-    print(f"{num3}, é maior")        
+    print(f"{num3} é maior")        
 
 
-
-
-if num1 <= num2 | num1 <= num3:
+if num1 <= num2 & num1 <= num3:
     print(f"{num1} é menor")
-
-elif num2 <= num1 | num2 <= num3:
-    print(f"{num2}, é menor")
-
+elif num2 <= num1 & num2 <= num3:
+    print(f"{num2} é menor")
 else:
-    print(f"{num3}, é menor")
+    print(f"{num3} é menor")
