28d04e3f593d5285d016b2d964698a54583b4332
Gabriel Mendes <gabriel.mendes.t0129859@edu.atec.pt>
2025-10-21 14:05:53 +0100

    Modificações no código

--- Exercícios/PPT.py
+++ Exercícios/PPT.py
@@ -1,38 +1,41 @@
-usr1 = int(input("Jogador 1, escolha: \n1- Pedra\n2- Papel\n3- Tesoura\nR "))
-
-usr2 = int(input("Jogador 2, escolha: \n1- Pedra\n2- Papel\n3- Tesoura\nR "))
-
 saida = True
 
 while saida:
+    
+    usr1 = int(input("Jogador 1, escolha: \n1- Pedra\n2- Papel\n3- Tesoura\nR "))
+    usr2 = int(input("Jogador 2, escolha: \n1- Pedra\n2- Papel\n3- Tesoura\nR "))
+    
     match usr1:
         case 1:
             if usr2 == 1:
-                 print("Empate!! (Pedra x Pedra)")
+                 print("Empate!! (Pedra x Pedra)\n")
             elif usr2 == 2:
-                 print("Vitória do Jogador 2!! (Pedra x Papel)")
+                 print("Vitória do Jogador 2!! (Pedra x Papel)\n")
             elif usr2 == 3:
-                 print("Vitória do Jogador 1!! (Pedra x Tesoura)")
+                 print("Vitória do Jogador 1!! (Pedra x Tesoura)\n")
             else:
-                 print("O número escolhido pelo Jogador 2 está incorreto!!")
+                 print("O número escolhido pelo Jogador 2 está incorreto!!\n")
+                 saida = False
         case 2: 
             if usr2 == 1:
-                 print("Vitória do Jogador 1!! (Papel x Pedra)")
+                 print("Vitória do Jogador 1!! (Papel x Pedra)\n")
             elif usr2 == 2:
-                 print("Empate!! (Papel x Papel)")
+                 print("Empate!! (Papel x Papel)\n")
             elif usr2 == 3:
-                 print("Vitória do Jogador 2!! (Papel x Tesoura)")
+                 print("Vitória do Jogador 2!! (Papel x Tesoura)\n")
             else:
-                 print("O número escolhido pelo Jogador 2 está incorreto!!")
+                 print("O número escolhido pelo Jogador 2 está incorreto!!\n")
+                 saida = False
         case 3:
             if usr2 == 1:
-                 print("Vitória do Jogador 2!! (Tesoura x Pedra)")
+                 print("Vitória do Jogador 2!! (Tesoura x Pedra)\n")
             elif usr2 == 2:
-                 print("Vitória do Jogador 1!! (Tesoura x Papel)")
+                 print("Vitória do Jogador 1!! (Tesoura x Papel)\n")
             elif usr2 == 3:
-                 print("Empate!! (Tesoura x Tesoura)")
+                 print("Empate!! (Tesoura x Tesoura)\n")
             else:
-                 print("O número escolhido pelo Jogador 2 está incorreto!!")
+                 print("O número escolhido pelo Jogador 2 está incorreto!!\n")
+                 saida = False
         case _:
-            print("O número escolhido pelo Jogador 1 está incorreto!!")
+            print("O número escolhido pelo Jogador 1 está incorreto!!\n")
             saida = False
